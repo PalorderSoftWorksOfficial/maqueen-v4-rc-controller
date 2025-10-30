cdac3e45d1f35c13c504d044d5c3d343bf54ff3c
PalorderSoftWorksOfficial <palordersoftworks@gmail.com>
2025-10-30 09:28:53 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,5 +1,11 @@
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    radio.sendString("stop")
+})
 input.onButtonPressed(Button.A, function () {
     radio.sendString("left")
+})
+input.onGesture(Gesture.Shake, function () {
+    radio.sendString("backwards")
 })
 input.onButtonPressed(Button.AB, function () {
     radio.sendString("forward")
@@ -7,4 +13,14 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     radio.sendString("right")
 })
-radio.setGroup(0)
+radio.setGroup(1)
+basic.forever(function () {
+    if (control.deviceSerialNumber() % 2 == 0) {
+        radio.setGroup(1)
+    } else {
+        radio.setGroup(1)
+        radio.onReceivedString(function (name) {
+        basic.showString(name)
+    })
+    }
+})
