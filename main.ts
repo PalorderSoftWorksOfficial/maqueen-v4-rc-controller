fea980636559db57ac0b7013bce7b743aa0de5df
PalorderSoftWorksOfficial <palordersoftworks@gmail.com>
2026-01-28 12:04:33 +0100

    pxt.json, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,25 +1,24 @@
-
 PacketLib.onReceivePacket(function (pkt) {
     let msg = PacketLib.bufferToString(pkt.payload)
     basic.showString(msg)
 })
 
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
-    PacketLib.sendPacket(0, pins.fromString("stop"), "all", PacketLib.PacketFlags.AckRequired)
+    PacketLib.sendPacket(0, PacketLib.stringToBuffer("stop"), "all", PacketLib.PacketFlags.AckRequired)
 })
 
 input.onButtonPressed(Button.A, function () {
-    PacketLib.sendPacket(0, pins.fromString("left"), "all", PacketLib.PacketFlags.AckRequired)
+    PacketLib.sendPacket(0, PacketLib.stringToBuffer("left"), "all", PacketLib.PacketFlags.AckRequired)
 })
 
 input.onGesture(Gesture.Shake, function () {
-    PacketLib.sendPacket(0, pins.fromString("backwards"), "all", PacketLib.PacketFlags.AckRequired)
+    PacketLib.sendPacket(0, PacketLib.stringToBuffer("backwards"), "all", PacketLib.PacketFlags.AckRequired)
 })
 
 input.onButtonPressed(Button.AB, function () {
-    PacketLib.sendPacket(0, pins.fromString("forward"), "all", PacketLib.PacketFlags.AckRequired)
+    PacketLib.sendPacket(0, PacketLib.stringToBuffer("forward"), "all", PacketLib.PacketFlags.AckRequired)
 })
 
 input.onButtonPressed(Button.B, function () {
-    PacketLib.sendPacket(0, pins.fromString("right"), "all", PacketLib.PacketFlags.AckRequired)
+    PacketLib.sendPacket(0, PacketLib.stringToBuffer("right"), "all", PacketLib.PacketFlags.AckRequired)
 })
